87ece724174f0b34a4427341d831bb88aed33046
singhaniya0411 <152794577+singhaniya0411@users.noreply.github.com>
2023-12-06 23:33:19 +0530

    sjf

--- 1_line_search_py.py
+++ 1_line_search_py.py
@@ -1,31 +1,90 @@
+#include<iostream>
 
-import numpy as np
+using namespace std;
 
-# Define the objective function you want to optimize
-def objective_function(x):
-    return x**2 + 2*x + 1  # Example: f(x) = x^2 + 2x + 1
+// Structure to represent a process
+struct Process {
+    int processID;
+    int arrivalTime;
+    int burstTime;
+    int completionTime;
+    int turnaroundTime;
+    int waitingTime;
+};
 
-# Define the derivative of the objective function
-def derivative(x):
-    return 2*x + 2  # Example: f'(x) = 2x + 2
+// Function to perform SJF scheduling and calculate average turnaround and waiting time
+void sjfScheduling(Process processes[], int n) {
+    // Sort processes based on arrival time
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = 0; j < n - i - 1; ++j) {
+            if (processes[j].arrivalTime > processes[j + 1].arrivalTime) {
+                // Swap processes
+                Process temp = processes[j];
+                processes[j] = processes[j + 1];
+                processes[j + 1] = temp;
+            }
+        }
+    }
 
-# Line Search method for optimization
-def line_search_optimization(learning_rate, initial_x, num_iterations):
-    x = initial_x
-    for i in range(num_iterations):
-        gradient = derivative(x)
-        x = x - learning_rate * gradient
-    return x
+    int currentTime = 0; // Current time
+    float totalTurnaroundTime = 0;
+    float totalWaitingTime = 0;
 
-# Initial guess and hyperparameters
-initial_guess = 0.0
-learning_rate = 0.1
-num_iterations = 100
+    for (int i = 0; i < n; ++i) {
+        // Find process with the shortest burst time among arrived processes
+        int shortestJobIndex = i;
+        for (int j = i + 1; j < n && processes[j].arrivalTime <= currentTime; ++j) {
+            if (processes[j].burstTime < processes[shortestJobIndex].burstTime) {
+                shortestJobIndex = j;
+            }
+        }
 
-# Perform Line Search optimization
-optimal_solution = line_search_optimization(learning_rate, initial_guess, num_iterations)
+        // Swap processes
+        Process temp = processes[i];
+        processes[i] = processes[shortestJobIndex];
+        processes[shortestJobIndex] = temp;
 
-# Print the result
-print("Optimal Solution:", optimal_solution)
-print("Objective Value at Optimal Solution:", objective_function(optimal_solution))
+        // Execute the process
+        processes[i].completionTime = currentTime + processes[i].burstTime;
+        processes[i].turnaroundTime = processes[i].completionTime - processes[i].arrivalTime;
+        processes[i].waitingTime = processes[i].turnaroundTime - processes[i].burstTime;
+
+        // Update current time
+        currentTime = processes[i].completionTime;
+
+        // Accumulate turnaround and waiting times
+        totalTurnaroundTime += processes[i].turnaroundTime;
+        totalWaitingTime += processes[i].waitingTime;
+    }
+
+    // Calculate average turnaround and waiting times
+    float avgTurnaroundTime = totalTurnaroundTime / n;
+    float avgWaitingTime = totalWaitingTime / n;
+
+    cout << "\nAverage Turnaround Time: " << avgTurnaroundTime << " units\n";
+    cout << "Average Waiting Time: " << avgWaitingTime << " units\n";
+}
+
+int main() {
+    int n;
+
+    // Input the number of processes
+    cout << "Enter the number of processes: ";
+    cin >> n;
+
+    // Input arrival time and burst time for each process
+    Process processes[n];
+    for (int i = 0; i < n; ++i) {
+        cout << "Enter arrival time for Process " << i + 1 << ": ";
+        cin >> processes[i].arrivalTime;
+        cout << "Enter burst time for Process " << i + 1 << ": ";
+        cin >> processes[i].burstTime;
+        processes[i].processID = i + 1;
+    }
+
+    // Perform SJF scheduling and calculate average turnaround and waiting times
+    sjfScheduling(processes, n);
+
+    return 0;
+}
 
